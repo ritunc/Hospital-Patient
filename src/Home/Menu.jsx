2937462d8cf75e4622b1731fa7564629de587ebd
Ritun Chakraborty <mithynead@gmail.com>
2024-10-06 10:25:39 +0530

    31th-commite

--- src/Home/Menu.jsx
+++ src/Home/Menu.jsx
@@ -8,14 +8,15 @@ import { Link } from 'react-router-dom';
 console.log(MenuItem);
 
 const Menu = () => {
-     
+        const auth_data = document.cookie;
+        console.log("auth_datasss:", auth_data);
         const {h1, h2, h3, logIn, email, pass, createAcc} = MenuItem;
 
         const ref1 = useRef(null);
         const ref2 = useRef(null);
 
         const [userlogin, setUserlogin] = useState({ email: "", password: ""});
-     
+        userlogin = auth_data
         let name, value;
         const AdminUserLogin = (e) => {
                 name = e.target.name;
@@ -40,7 +41,7 @@ const Menu = () => {
                                 headers:{
                                         "Content-Type":"application/json",
                                 },
-                                body:JSON.stringify(userlogin),
+                                body:JSON.stringify(userlogin, auth_data),
                         })
                         .then(res => res.json())
                         .then(data => {
