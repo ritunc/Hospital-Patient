38c9c5490fdf7166e177e741eb4a9a0b23f8f515
Ritun Chakraborty <mithynead@gmail.com>
2025-04-20 02:42:49 +0530

    Admin.Jsx UI/UX improve

--- src/Admin/menu.jsx
+++ src/Admin/menu.jsx
@@ -10,44 +10,105 @@ const Menu = () => {
 
         return (
                 <>
-                <main className='adminMain'>
-                <div className="adminimgContainer">
-                        <h1 className="text">Welcome to Med Stack</h1>
-                        <h2 className="text">Share your Info</h2>
-                        <h3 className="text">Protect Info</h3>
-                </div>
-                <div className="logIn">
-                        <h1 id="admin">Admin</h1>
-                        <form method='POST'>
-                                <div className="formContain">
-                                        <label>Name:</label>
-                                        <input type="text" name="name" required 
-                                        value={contect.user.name}
-                                        onChange={contect.handleInputs} className='adminInput'/>
-                                </div>
+                        <main>
+                                <section className="container">
+                                        <div className="grid">
 
-                                <div className="formContain">
-                                        <label>Email:</label>
-                                        <input type="email" name="email" required 
-                                        value={contect.user.email}
-                                        onChange={contect.handleInputs} className='adminInput'/>
-                                </div>
+                                                <div className="card card-rotate">
 
-                                <div className="formContain">
-                                        <label>Password:</label>
-                                        <input type="password" name="password" required 
-                                        value={contect.user.password}
-                                        onChange={contect.handleInputs} className='adminInput'/>
-                                </div>
+                                                        <h1 className="Texts">Welcome to Med Stack</h1>
+                                                        <h2 className="Texts">Share your Info</h2>
+                                                        <h3 className="Texts">Protect Info</h3>
 
-                                <div className="btnformContain">
-                                        <button type="submit" onClick={contect.AdminSignUpPostData} id="btn">Submit</button>
+
+
+
+                                                </div>
+                                                <div className="card">
+                                                        <div className="LogIn">
+                                                                <h1 id="admins">Admin</h1>
+                                                                <form className='home-form'>
+                                                                        <div className="FormContains">
+                                                                                <label className='LABALS'>Name:</label>
+                                                                                {/* <input type="email" name="email" required value={userlogin.email} ref={ref1} onChange={AdminUserLogin} className='LogInputs' /> */}
+                                                                                <input type="text" name="name" required value={contect.user.name} onChange={contect.handleInputs} className='LogInputs' />
+
+                                                                        </div>
+
+                                                                        <div className="FormContains">
+                                                                                <label className='LABALS'>Email:</label>
+                                                                                {/* <input type="email" name="email" required value={userlogin.email} ref={ref1} onChange={AdminUserLogin} className='LogInputs' /> */}
+                                                                                <input type="email" name="email" required value={contect.user.email} onChange={contect.handleInputs} className='LogInputs' />
+                                                                        </div>
+
+                                                                        <div className="FormContains">
+                                                                                <label className='LABALS'>Password:</label>
+                                                                                {/* <input type="password" name="password" required value={userlogin.password} ref={ref2} onChange={AdminUserLogin} className='LogInputs' /> */}
+                                                                                <input type="password" name="password" required value={contect.user.password} onChange={contect.handleInputs} className='LogInputs' />
+                                                                        </div>
+
+                                                                        <div className="btnFormContain">
+                                                                                {/* <button type="submit" onClick={AdminloginPostData} id="btns">{logIn}</button> */}
+                                                                                <button type="submit" onClick={contect.AdminSignUpPostData} id="btns">Submit</button>
+                                                                        </div>
+                                                                </form>
+
+                                                        </div>
+                                                </div>
+
+
+                                        </div>
+
+                                </section>
+
+                        </main>
+
+                        <footer>
+                                <div className="container">
+                                        <p>&copy; 2023 Your Company. All rights reserved.</p>
                                 </div>
-                        </form>
-                </div>
-        </main>
+                        </footer>
+
                 </>
         )
 }
 
 export default Menu
+
+
+{/* <main className='adminMain'>
+<div className="adminimgContainer">
+        <h1 className="text">Welcome to Med Stack</h1>
+        <h2 className="text">Share your Info</h2>
+        <h3 className="text">Protect Info</h3>
+</div>
+<div className="logIn">
+        <h1 id="admin">Admin</h1>
+        <form method='POST'>
+                <div className="formContain">
+                        <label>Name:</label>
+                        <input type="text" name="name" required 
+                        value={contect.user.name}
+                        onChange={contect.handleInputs} className='adminInput'/>
+                </div>
+
+                <div className="formContain">
+                        <label>Email:</label>
+                        <input type="email" name="email" required 
+                        value={contect.user.email}
+                        onChange={contect.handleInputs} className='adminInput'/>
+                </div>
+
+                <div className="formContain">
+                        <label>Password:</label>
+                        <input type="password" name="password" required 
+                        value={contect.user.password}
+                        onChange={contect.handleInputs} className='adminInput'/>
+                </div>
+
+                <div className="btnformContain">
+                        <button type="submit" onClick={contect.AdminSignUpPostData} id="btn">Submit</button>
+                </div>
+        </form>
+</div>
+</main> */}
